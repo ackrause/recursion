efc88b9cd65083f995a261b64f2cbdccc1d73ce2
Andrew Krause <a.c.krause@gmail.com>
2014-03-31 12:57:43 -0700

    Converts numeric types to JSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,24 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  var type = typeof(obj);
+  var json = '';
+
+  // Stringifying functions by obj type
+  var number = function(val) {
+    return isNaN(val) ? 'null' : String(val);
+  };
+
+  // This is better than a switch statement??
+  var stringify = {
+    'number': number
+  };
+
+  if (typeof(stringify[type]) === 'function') {
+    json = stringify[type](obj);
+  } else {
+    json = '';
+  }
+
+  return json;
 };
